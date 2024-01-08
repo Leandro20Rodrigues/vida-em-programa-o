1b9153fcf3038cbaae8ecbf36f0afe5adcc87f24
Leandro20Rodrigues <154839952+Leandro20Rodrigues@users.noreply.github.com>
2024-01-08 19:57:07 -0300

    Add files via upload

---
+++ TrabalhoNoSupermercado.js
@@ -0,0 +1,4 @@
+//De manhã eu vou trabalhar no supermercado, trabalho até as 18 horas da tarde.
+let trabalho = ("supermercado");
+let horario = 18;
+console.log (`Vou trabalhar no ${trabalho} de manhã e saio de tarde as: ${horario} horas da tarde`);
